7c2f3cab0e1514b3595f2bb28cc3f7eeb68d968e
striveries <121800213+striveries@users.noreply.github.com>
2024-06-14 03:16:10 +0700

    still has some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <ButtonGradient />
+        <Hero />
       </div>
       <h1>hey</h1>
+      <ButtonGradient />
     </>
   );
 };
